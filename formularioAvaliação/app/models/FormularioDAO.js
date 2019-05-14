6167201305fb9ca1b038756d41eead65a5ba13e7
elvis thermo <elvisthermo@hotmail.com>
2019-05-14 13:03:30 -0300

    agora está gravando os objetos no mongodb

--- formularioAvaliação/app/models/FormularioDAO.js
+++ formularioAvaliação/app/models/FormularioDAO.js
@@ -25,7 +25,7 @@ FormularioDAO.prototype.autenticar = function(usuario,dadosForm,req,res){
                     req.session.number = result[0].number;
                 }
                 if(req.session.number > 0 && dadosForm.number ==0){
-                    req.assert('nome',"usuário ja respondeu parte do formulario continue sua respostas até aqui forma salvas").notEmpty();
+                    req.assert('nome',"usuário ja respondeu parte do formulario continue sua respostas até aqui foram salvas").notEmpty();
                     let erros = req.validationErrors();
                     res.render('index', { validacao:erros, dadosForm: result,number:req.session.number, _id:req.session.usuario});
                 }else if(req.session.autorizado){
@@ -49,34 +49,47 @@ FormularioDAO.prototype.cadatrarQuest = function(usuario,form,req,res){
                     req.session._id = result[0]._id;
                     req.session._number = result[0].number;
                 }
-                let quest =  req.session._number;
-                switch(form.number) {
-                    case '1':
-                        req.assert('quest1', 'A questão deve ser respondida').notEmpty();
-                        collection.update({_id:result[0]._id}, {$set:{quest1:form.tipo0, number:form.number}});
+
+                var target = "id";
+                var field = form.number;
+
+                var obj = {};
+                obj[field] = target;
+
+
+
+
+                console.log("obj:",obj);
+
+                let cont_number = form.number;
+                console.log("formulario:",form);
+                // let quest =  req.session._number;
+                // switch(form.number) {
+                    // case '1':
+                        collection.update({_id:result[0]._id},{$set:{[form.number]:form, number:form.number}},{"upsert":true});
                         res.render('index', { validacao:{},form:{},number:parseInt(form.number)+1, _id:req.session.usuario});
-                        break;
-                    case '2':
-                        collection.update({_id:result[0]._id}, {$set:{quest2:form.tipo1, number:form.number}});
-                        res.render('index', { validacao:{},form:{},number:parseInt(form.number)+1, _id:req.session.usuario });
-                        break;
-                    case '3':
-                        collection.update({_id:result[0]._id}, {$set:{quest3:form.tipo2, number:form.number}});
-                        res.render('index', { validacao:{},form:{},number:parseInt(form.number)+1, _id:req.session.usuario });
-                        break;
-                    case '4':
-                        collection.update({_id:result[0]._id}, {$set:{quest4:form.tipo3, number:form.number}});
-                        res.render('index', { validacao:{},form:{},number:parseInt(form.number)+1, _id:req.session.usuario });
-                        break;
-                    case '5':
-                        collection.update({_id:result[0]._id}, {$set:{quest5:form.tipo4, number:form.number}});
-                        res.render('index', { validacao:{},form:{},number:parseInt(form.number)+1, _id:req.session.usuario });
-                        break;
-                    default:
-                        res.render('end');
-                        console.log("Formulario finalizado");
-                        break;
-                }
+                //         break;
+                //     case '2':
+                //         collection.update({_id:result[0]._id}, {$set:{frase1:form, number:form.number}});
+                //         res.render('index', { validacao:{},form:{form},number:parseInt(form.number)+1, _id:req.session.usuario });
+                //         break;
+                //     case '3':
+                //         collection.update({_id:result[0]._id}, {$set:{frase3:form, number:form.number}});
+                //         res.render('index', { validacao:{},form:{form},number:parseInt(form.number)+1, _id:req.session.usuario });
+                //         break;
+                //     case '4':
+                //         collection.update({_id:result[0]._id}, {$set:{frase4:form, number:form.number}});
+                //         res.render('index', { validacao:{},form:{form},number:parseInt(form.number)+1, _id:req.session.usuario });
+                //         break;
+                //     case '5':
+                //         collection.update({_id:result[0]._id}, {$set:{frase5:form, number:form.number}});
+                //         res.render('index', { validacao:{},form:{form},number:parseInt(form.number)+1, _id:req.session.usuario });
+                //         break;
+                //     default:
+                //         res.render('end');
+                //         console.log("Formulario finalizado");
+                //         break;
+                // }
 
             });
         });
